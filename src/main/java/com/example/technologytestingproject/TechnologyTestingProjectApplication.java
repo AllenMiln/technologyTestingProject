c87579897928717735b2f5e772ba7958aa008adb
20113571 <SAButylchenko@sberbank.ru>
2022-07-29 14:11:17 +0300

    MASTER-000 - new basic project

---
+++ src/main/java/com/example/technologytestingproject/TechnologyTestingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.technologytestingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyTestingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnologyTestingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/technologytestingproject/TechnologyTestingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.technologytestingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologyTestingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
